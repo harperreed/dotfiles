a4fce8d384cd4d36f77fda93be4c58456480d4ca
Harper Reed <harper@nata2.org>
2021-01-15 12:01:54 -0600

    updated fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,7 @@ set -g theme_display_git_ahead_verbose yes
 set -g theme_display_git_dirty_verbose yes
 set -g theme_display_git_master_branch yes
 set -g theme_git_worktree_support yes
-set -g theme_display_vagrant no 
+set -g theme_display_vagrant no
 set -g theme_display_docker_machine no
 set -g theme_display_k8s_context yes
 set -g theme_display_hg yes
@@ -14,20 +14,20 @@ set -g theme_display_ruby no
 set -g theme_display_user ssh
 set -g theme_display_hostname ssh
 set -g theme_display_vi yes
-set -g theme_display_date yes 
+set -g theme_display_date yes
 set -g theme_display_cmd_duration yes
 set -g theme_title_display_process yes
-set -g theme_title_display_path yes 
+set -g theme_title_display_path yes
 set -g theme_title_display_user yes
-set -g theme_title_use_abbreviated_path yes 
+set -g theme_title_use_abbreviated_path yes
 set -g theme_date_format "+%a %H:%M"
 set -g theme_avoid_ambiguous_glyphs yes
 set -g theme_powerline_fonts yes
-set -g theme_nerd_fonts no 
+set -g theme_nerd_fonts no
 set -g theme_show_exit_status yes
 set -g default_user your_normal_user
-set -g theme_color_scheme terminal2-dark-black 
-set -g fish_prompt_pwd_dir_length 20 
+set -g theme_color_scheme terminal2-dark-black
+set -g fish_prompt_pwd_dir_length 20
 set -g theme_project_dir_length 1
 set -g theme_newline_cursor yes
 
@@ -50,7 +50,7 @@ for i in (cat ~/.secrets/secrets.env)
 end
 
 function fish_greeting
-    hostname | figlet |lolcat
+    hostname | figlet -f "~/.config/fonts/Bloody.flf" |lolcat
     fortune |lolcat
 echo
 echo
@@ -65,5 +65,3 @@ source ~/.config/fish/fish_aliases
 source ~/.config/fish/fish_paths
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/harper/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/home/harper/Downloads/google-cloud-sdk/path.fish.inc'; end
-
-
