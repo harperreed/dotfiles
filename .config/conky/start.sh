ee7bed5c82f849f60252bde7a897b1eb7f721e7d
Harper Reed <harper@nata2.org>
2021-10-13 14:31:29 -0500

    updated start script

--- .config/conky/start.sh
+++ .config/conky/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 /usr/bin/conky -c /home/harper/.conky/clock.conky.conf
-/usr/bin/conky -c /home/harper/.conky/cal.conky.conf
+## nope /usr/bin/conky -c /home/harper/.conky/cal.conky.conf
+/usr/bin/conky -c /home/harper/.conky/ethereum.conky.conf
 /usr/bin/conky
