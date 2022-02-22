f03a6d99ba3886c0daae03c8dcc068a035db6abf
Harper Reed <harper@nata2.org>
2022-02-22 11:25:41 -0600

    various tweaks from linux

--- .bash_profile
+++ .bash_profile
@@ -124,3 +124,5 @@ fortune |lolcat
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+-e 
+if [ -e /home/harper/.nix-profile/etc/profile.d/nix.sh ]; then . /home/harper/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
--- .config/conky/ethprice.py
+++ .config/conky/ethprice.py
@@ -9,6 +9,10 @@ url ='https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=u
 response = requests.get(url)
 data = response.json()
 
+url = 'http://companion.local:8000/style/bank/1/18?text=ETH+%s' % str(data['ethereum']['usd'])
+
+r = requests.get(url)
+
 
 print("${voffset 5}${font Open Sans Light:size=100}" + str(data['ethereum']['usd']) + " USD${font}${voffset -5}")
 print("${voffset 5}${font Open Sans Light:size=40}24h change: " + str(round(data['ethereum']['usd_24h_change'],1)) + "%${color}${voffset -5}")
