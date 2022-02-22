3099878590cf92f2ff9805fdea04a58ec04916d9
Harper Reed <harper@nata2.org>
2022-02-22 11:25:41 -0600

    various tweaks from linux

--- .config/conky/gastracker.py
+++ .config/conky/gastracker.py
@@ -6,5 +6,8 @@ r = requests.get(url)
 
 gas = r.json()
 
+url = 'http://companion.local:8000/style/bank/1/19?text=%s+gwei' % str(gas['average']/10)
+
+r = requests.get(url)
 
 print(gas['average'] / 10)
