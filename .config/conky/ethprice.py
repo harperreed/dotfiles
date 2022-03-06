54fb99909b323c1a77d75d47de6317ebf90ffe6b
Harper Reed <harper@nata2.org>
2022-03-06 10:32:50 -0600

    updated from million

--- .config/conky/ethprice.py
+++ .config/conky/ethprice.py
@@ -1,17 +1,37 @@
 import requests
 
+import color_gen
 
-
+max_eth_price = 4500
+offset = 1500
 
 url ='https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd&include_24hr_change=true&include_last_updated_at=true'
 # url ='https://api.coingecko.com/api/v3/coins/list'
 
 response = requests.get(url)
 data = response.json()
+bgcolor = "000000"
+color = "000000"
 
-url = 'http://companion.local:8000/style/bank/1/18?text=ETH+%s' % str(data['ethereum']['usd'])
+num_colors = max_eth_price - offset
+two_stop_bg = color_gen.pd_linear_gradient(start_hex = '#EF3A37', finish_hex = '#45EF37', n = num_colors)
+fgarray = two_stop_bg[::-1]
 
-r = requests.get(url)
+color_pos = int(data['ethereum']['usd']) - offset
+if (color_pos<0):
+  color_pos = 0
+
+if (data['ethereum']['usd'] < max_eth_price):
+  bgcolor = color_gen.RGB_to_hex(two_stop_bg.iloc[color_pos]).replace("#", "")
+else:
+  bgcolor = "45EF37"
+
+
+try:
+  url = 'http://companion.local:8000/style/bank/1/18?text=ETH+%s&bgcolor=%s&&color=%s' % (str(data['ethereum']['usd']), bgcolor, color)
+  r = requests.get(url)
+except:
+  pass
 
 
 print("${voffset 5}${font Open Sans Light:size=100}" + str(data['ethereum']['usd']) + " USD${font}${voffset -5}")
--- .config/conky/gastracker.py
+++ .config/conky/gastracker.py
@@ -1,4 +1,5 @@
 import requests
+import color_gen
 
 url = 'https://ethgasstation.info/api/ethgasAPI.json'
 
@@ -6,8 +7,21 @@ r = requests.get(url)
 
 gas = r.json()
 
-url = 'http://companion.local:8000/style/bank/1/19?text=%s+gwei' % str(gas['average']/10)
+two_stop_bg = color_gen.pd_linear_gradient(start_hex = '#45EF37', finish_hex = '#EF3A37', n = 300)
+# fgarray = two_stop_bg[::-1]
 
-r = requests.get(url)
+fgcolor = "000000"
+
+if ((gas['average']/10)<150):
+    fgcolor = "FFFFFF"
+
+# fgcolor = color_gen.RGB_to_hex(fgarray.iloc[int(gas['average']/10)]).replace("#", "")
+
+bgcolor = color_gen.RGB_to_hex(two_stop_bg.iloc[int(gas['average']/10)]).replace("#", "")
+try:
+  url = 'http://companion.local:8000/style/bank/1/19?text=%s+gwei&bgcolor=%s&&color=%s' % (str(gas['average']/10), bgcolor, fgcolor)
+  r = requests.get(url)
+except:
+  pass
 
 print(gas['average'] / 10)
