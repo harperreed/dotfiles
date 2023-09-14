4824b84408643e762e1f7ecc6e5f7b504f369708
Harper Reed <harper@nata2.org>
2023-09-13 21:56:21 -0500

    updated fish variables and iterm goodies

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -85,8 +85,8 @@ test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shel
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /Users/harper/miniconda3/bin/conda
-    eval /Users/harper/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+if test -f /Users/harper/.miniconda3/bin/conda
+    eval /Users/harper/.miniconda3/bin/conda "shell.fish" "hook" $argv | source
 end
 # <<< conda initialize <<<
 
