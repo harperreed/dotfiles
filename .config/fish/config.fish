1243a73ccab3c0235f20bdaeee4cf78eeb1021a3
Harper Reed <harper@nata2.org>
2024-03-07 20:34:51 -0600

    updated conda

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -85,8 +85,14 @@ test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shel
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /Users/harper/.miniconda3/bin/conda
-    eval /Users/harper/.miniconda3/bin/conda "shell.fish" "hook" $argv | source
+if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+        . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+    end
 end
 # <<< conda initialize <<<
 
