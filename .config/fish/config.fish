f9a4ec5354dd48c2a892fbd591810b4e74237a76
Harper Reed <harper@nata2.org>
2024-07-15 15:43:45 -0500

    🚀 Overhauled fisher functions and config - Replaced old fisher config and functions with updated versions - fisher function condensed and modernized - nvm function updated to latest version - Removed outdated fisher plugins and associated functions:   - gitnow   - pyenv   - wd   - docker/docker-compose completions   - spin   - bax   - fish-apple-touchbar - Updated done plugin to latest version 🎉 Cleaned up config.fish and reorganized - Moved theme settings to separate theme.fish file - Secrets loading streamlined - Custom greeting function improved with more robust checks - Reorganized config into logical sections:   - PATH setup   - OS-specific configs   - Prompt (Starship?) setup   - Key framework inits: Atuin, ASDF   - SSH agent setup - Removed old/unused PATH entries and set more through Universal variables at the end 🔥 Removed old unused functions and simplified - Deleted old custom functions for gitnow, wd, spin, etc. - Removed old unused completions and abbr (docker, git, etc.) 💅 Styling and organization improvements - More emojis and better headings/sections - Consistent indentation and alignment throughout files - Removed crufty old aliases and simplified PATH setup
    Overall, did a deep clean and refresh of all the fish config and functions. Aiming for a leaner and cleaner setup that is easier to understand, troubleshoot and extend! Let me know if you have any other questions.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,104 +1,63 @@
-set -g theme_display_git yes
-set -g theme_display_git_dirty no
-set -g theme_display_git_untracked no
-set -g theme_display_git_ahead_verbose yes
-set -g theme_display_git_dirty_verbose yes
-set -g theme_display_git_master_branch yes
-set -g theme_git_worktree_support yes
-set -g theme_display_vagrant no
-set -g theme_display_docker_machine no
-set -g theme_display_k8s_context yes
-set -g theme_display_hg yes
-set -g theme_display_virtualenv yes
-set -g theme_display_ruby no
-set -g theme_display_user ssh
-set -g theme_display_hostname ssh
-set -g theme_display_vi yes
-set -g theme_display_date yes
-set -g theme_display_cmd_duration yes
-set -g theme_title_display_process yes
-set -g theme_title_display_path yes
-set -g theme_title_display_user yes
-set -g theme_title_use_abbreviated_path yes
-set -g theme_date_format "+%a %H:%M"
-set -g theme_avoid_ambiguous_glyphs yes
-set -g theme_powerline_fonts yes
-set -g theme_nerd_fonts no
-set -g theme_show_exit_status yes
-set -g default_user your_normal_user
-set -g theme_color_scheme terminal2-dark-black
-set -g fish_prompt_pwd_dir_length 20
-set -g theme_project_dir_length 1
-set -g theme_newline_cursor yes
+# Fish shell main configuration
 
-# Turn off the prompt for virtualenv
-set -x VIRTUAL_ENV_DISABLE_PROMPT 1
+# Load theme settings
+source ~/.config/fish/theme.fish
 
-# Strap Fisher up in case we haven't installed our plugins
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-
-# Load secrets into memory to stop them being present in my config dotfiles like fish_variables
+# Load secrets
 for i in (cat ~/.secrets/secrets.env)
-        if test (echo $i | sed -E 's/^[[:space:]]*(.).+$/\\1/g') != "#"
-                set arr (echo $i |tr = \n)
-                set -gx $arr[1] $arr[2]
-        end
-end
-
-function fish_greeting
-    hostname -s | figlet -w 100 -f ~/.config/fonts/Bloody.flf |lolcat
-    fortune |lolcat
-echo
-echo
-end
-
-if type -q kitty
-    kitty + complete setup fish | source
-end
-
-
-source ~/.config/fish/fish_aliases
-source ~/.config/fish/fish_paths
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/home/harper/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/home/harper/Downloads/google-cloud-sdk/path.fish.inc'; end
-
-export PATH="$PATH:/Users/harper/.foundry/bin"
-
-
-switch (uname)
-    case Linux
-            echo Hi Tux!
-    case Darwin
-            set -gx PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
-            set -gx PUPPETEER_EXECUTABLE_PATH (which chromium)
-    case FreeBSD NetBSD DragonFly
-            echo Hi Beastie!
-    case '*'
-            echo Hi, stranger!
-end
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
-    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
-        . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+    if test (echo $i | sed -E 's/^[[:space:]]*(.).+$/\\1/g') != "#"
+        set arr (echo $i |tr = \n)
+        set -gx $arr[1] $arr[2]
     end
 end
-# <<< conda initialize <<<
 
+# Custom greeting
+function fish_greeting
+    set -l hostname_cmd (command -v hostname)
+    set -l figlet_cmd (command -v figlet)
+    set -l lolcat_cmd (command -v lolcat)
 
-# Created by `pipx` on 2023-05-23 19:16:10
-set PATH $PATH /Users/harper/.local/bin
+    if test -n "$hostname_cmd" -a -n "$figlet_cmd" -a -n "$lolcat_cmd"
+        # Use command to bypass any GRC wrappers
+        command $hostname_cmd -s | command $figlet_cmd -w 100 -f ~/.config/fonts/Bloody.flf | $lolcat_cmd
+        command fortune | $lolcat_cmd
+    else
+        echo "Welcome!"
+    end
+    echo
+    echo
+end
+
+# Source aliases and paths
+source ~/.config/fish/aliases.fish
+source ~/.config/fish/paths.fish
+
+# Platform-specific configurations
+switch (uname)
+    case Linux
+        echo Hi Tux!
+    case Darwin
+        set -gx PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
+        set -gx PUPPETEER_EXECUTABLE_PATH (which chromium)
+    case FreeBSD NetBSD DragonFly
+        echo Hi Beastie!
+    case '*'
+        echo Hi, stranger!
+end
+
+# iTerm2 integration
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+# Conda initialization
+if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+else if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+    source "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+else
+    set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+end
+
+# Go configuration
 switch (uname -m)
     case 'x86_64'
         set GOPATH /home/harper/go/
@@ -106,22 +65,60 @@ switch (uname -m)
         set GOPATH /Users/harper/workspace/personal/go
 end
 
+# Atuin shell history
 if status is-interactive
-  atuin init fish  | source
+    atuin init fish | source
 end
 
+# ASDF version manager
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
-
+# SSH agent configuration
 if not set -q SSH_AUTH_SOCK
     eval (ssh-agent -c)
     set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
     set -Ux SSH_AGENT_PID $SSH_AGENT_PID
 end
 
-# Function to add keys on demand
+# Function to add SSH keys on demand
 function add_ssh_keys
     ssh-add --apple-use-keychain ~/.ssh/id_rsa
     ssh-add --apple-use-keychain ~/.ssh/id_ed25519
     # Add more keys as needed
 end
+
+# Load custom functions
+for f in ~/.config/fish/functions/*.fish
+    source $f
+end
+
+
+
+# Set default editors
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
+# Set language and locale
+set -gx LANG en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
+
+# Set PATH
+set -gx PATH $HOME/.local/bin $PATH
+set -gx PATH /usr/local/sbin $PATH
+
+# Python settings
+set -gx PYTHONDONTWRITEBYTECODE 1
+
+# Java settings
+set -gx JAVA_HOME (/usr/libexec/java_home)
+
+# Node.js settings
+set -gx NODE_ENV development
+
+# Go settings
+set -gx GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+
+
+# Rust settings
+set -gx PATH $HOME/.cargo/bin $PATH
