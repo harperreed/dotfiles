ce3ab62220656c7286fca827bf3af4a11b8c7282
Harper Reed <harper@nata2.org>
2024-07-15 15:59:22 -0500

    🎉 Woohoo, looks like you added a bunch of cool new fish shell configuration! 🐠🎣  Let's break it down:
    📁 Added some handy aliases for navigation, shortcuts, file listing, system tasks, network stuff, and git in aliases.fish
    
    🐟 Set up autocomplete for fzf_configure_bindings in fzf_configure_bindings.fish
    
    🍺 Added a configuration to set up Homebrew paths in brew.fish
    
    📜 Created a list of executable names in executables
    
    🔑 Set up fish-ssh-agent for managing SSH keys in fish-ssh-agent.fish
    
    🔍 Configured fzf for fancy searching in fzf.fish
    
    🎨 Added configurations for the grc colorizer in grc.fish
    
    🚀 Set up the z plugin for fast directory jumping in z.fish
    
    🐠 Added your list of desired fish plugins in fish_plugins
    
    🐍 Added a Python script to support the bass plugin in __bass.py
    
    So overall, you've got a pretty sweet setup now with some powerful plugins and tools to supercharge your fish shell experience! 😎 The aliases and helper functions should make your day-to-day tasks a lot smoother. Happy fishing! 🎣
    
    To install these changes, just run:
    `fisher update`
    
    And you should be good to go! Let me know if you need any other help. 💪

---
+++ .config/fish/paths.fish
@@ -0,0 +1,9 @@
+# Fish shell PATH configurations
+
+set -gx PATH $HOME/.npm-global/bin $PATH
+set -gx PATH $HOME/.config/bin $PATH
+set -gx PATH /opt/homebrew/bin $PATH
+set -gx PATH $HOME/go/bin $PATH
+set -gx PATH $HOME/.local/bin $PATH
+
+# Add more PATH configurations here if needed
