ecd5c922b844428aeebcb31b2162ff98ce679454
Harper Reed <harper@nata2.org>
2025-12-31 23:27:51 +0000

    Some tweaks fromlinux land

--- .config/fish/conf.d/prek.env.fish
+++ .config/fish/conf.d/prek.env.fish
@@ -1,2 +1,5 @@
-
-source "$HOME/.local/share/../bin/env.fish"
+# ABOUTME: Sources prek environment setup if available
+# ABOUTME: Only loads if the env file exists on this system
+if test -f "$HOME/.local/bin/env.fish"
+    source "$HOME/.local/bin/env.fish"
+end
