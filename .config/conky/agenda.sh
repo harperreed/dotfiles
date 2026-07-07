93ddb33c36f4ec592a2756ba2ce02992835cc3b5
Harper Reed <harper@nata2.org>
2026-07-07 10:57:56 -0500

    chore: remove dead configs from the GNOME/Pop era (2020-2022)
    pop-shell, terminator, dconf dump, alacritty, beets, theia, vscode,
    conky + its autostart launchers. Nothing else references them and none
    have been touched in 3-6 years. HL2DM config stays, by decree.

--- .config/conky/agenda.sh
+++
@@ -1,1 +0,0 @@
-gcalcli --conky --calendar "harper@galactic.io"#blue --calendar "harper@nata2.org"#white agenda >/tmp/gcalcli_agenda.txt
--- .config/conky/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-/usr/bin/conky -c /home/harper/.config/conky/clock.conky.conf
-## nope /usr/bin/conky -c /home/harper/.conky/cal.conky.conf
-/usr/bin/conky -c /home/harper/.config/conky/ethereum.conky.conf
-/usr/bin/conky
